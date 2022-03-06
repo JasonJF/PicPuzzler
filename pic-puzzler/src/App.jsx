7ab876699d5a8262f608b3fcc63f55a96c143fda
JasonJF <47773807+JasonJF@users.noreply.github.com>
2022-03-06 14:48:45 +0200

    feat: Setup heroku

--- pic-puzzler/src/App.jsx
+++ pic-puzzler/src/App.jsx
@@ -14,31 +14,36 @@ function App() {
   }
   return (
     <div className="App">
-      <div class="container pageWrapper">
+      <div className="container pageWrapper">
         {/* Navigation */}
-        <nav class="navbar myNav center">
+        <nav className="navbar myNav center">
           <p>PicPuzzler</p>
         </nav>
         {/* Main Content */}
-        <div class="mainContent">
-          <div class="scoreWrapper center"> Score : 0</div>
-          <div class="gameSquare">
-            <div id="tileContainer" class="tileContainer">
-              {tiles.map((tile) => {
+        <div className="mainContent">
+          <div className="scoreWrapper center"> Score : 0</div>
+          <div className="gameSquare">
+            <div id="tileContainer" className="tileContainer">
+              {tiles.map((tile, index) => {
                 return (
-                  <canvas id={tile} class="tile" onClick={handleTileClick}>
+                  <canvas
+                    key={index}
+                    id={tile}
+                    className="tile"
+                    onClick={handleTileClick}
+                  >
                     {tile}
                   </canvas>
                 );
               })}
             </div>
           </div>
-          <div class="gameButtons">
-            <button class="btn btn-lg btn-warning">Reset</button>
+          <div className="gameButtons">
+            <button className="btn btn-lg btn-warning">Reset</button>
           </div>
         </div>
         {/* Footer */}
-        <div class="footer center">
+        <div className="footer center">
           <p>Copyright Jason JF</p>
         </div>
       </div>
