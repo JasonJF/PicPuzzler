3fa554f535d10c1390caaf869b73eef9045960ff
JasonJF <47773807+JasonJF@users.noreply.github.com>
2022-03-06 14:03:46 +0200

    feat: tiles can now move

--- pic-puzzler/src/scripts/gameLogic.js
+++ pic-puzzler/src/scripts/gameLogic.js
@@ -1,25 +1,58 @@
 //gameLogic.js
 //This file contains all the game logic functions
 
+//default position classes
+const position = {
+  p0: ["p1", "p3"],
+  p1: ["p0", "p2", "p4"],
+  p2: ["p1", "p5"],
+  p3: ["p0", "p4", "p6"],
+  p4: ["p1", "p3", "p5", "p7"],
+  p5: ["p2", "p4", "p8"],
+  p6: ["p3", "p7"],
+  p7: ["p4", "p6", "p8"],
+  p8: ["p5", "p7"],
+};
+
 //handleTileClick
 function handleTileClick(e) {
-  console.log(e.target);
+  let clickedTile = e.currentTarget.getAttribute("class");
+  // console.log(e.target);
+  let clickedClass = e.target.getAttribute("class");
+  // console.log(clickedClass);
+  //strip away the tile class
+  let trimClass = clickedClass.replace("tile ", "");
+  // console.log(trimClass);
+  //check adjacent tiles for blanks
+  let adjTiles = position[trimClass];
+  adjTiles.forEach((adjTile) => {
+    // console.log(adjTile);
+
+    if ($("." + adjTile).hasClass("blankTile")) {
+      let emptyTile = $("." + adjTile);
+      let tile = $("." + trimClass);
+      // console.log(adjTile + " is blank.");
+      //move the tile
+      moveTile(tile, emptyTile);
+    }
+  });
 }
 
 //handle tile movement
 function moveTile(tile, emptyTile) {
-  // let regex = \w\d;
-  let tileId = tile[0].id;
+  // console.log(tile);
+  // // let regex = \w\d;
+  // let tileId = tile[0].id;
   let tileClass = tile.attr("class").replace("tile ", "");
-  let emptyId = emptyTile[0].id;
-  let emptyClass = emptyTile.attr("class").match(/\w\d/);
-  //
-  console.log(tileId);
-  console.log(tileClass);
-  console.log(emptyId);
-  console.log(emptyClass);
+  // let emptyId = emptyTile[0].id;
+  let emptyClass = emptyTile.attr("class").match(/\w\d/)[0];
+  // //
+  // console.log(tileId);
+  // console.log(tileClass);
+  // console.log(emptyId);
+  // console.log(emptyClass);
 
-  //swap classes
+  // //swap classes
   tile.removeClass(tileClass);
   emptyTile.removeClass(emptyClass);
 
